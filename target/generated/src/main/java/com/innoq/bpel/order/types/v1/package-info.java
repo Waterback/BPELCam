990a69fc95f1cbf16d8b42517f95cb5b90965ffa
Martin Huber <martinh@innoq.com>
2012-02-09 00:32:55 +0100

    remove class-files

--- target/generated/src/main/java/com/innoq/bpel/order/types/v1/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://bpel.innoq.com/order/types/v1.0")
-package com.innoq.bpel.order.types.v1;
--- target/generated/src/main/java/com/innoq/bpel/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://bpel.innoq.com")
-package com.innoq.bpel;
