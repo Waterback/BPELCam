4400ea2e06b4c1b4ecec4c59878c76c10e5946a0
Martin Huber <martinh@innoq.com>
2012-02-15 01:00:10 +0100

    removed target

--- target/generated/src/main/java/com/innoq/bpel/order/v1/sv0/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://bpel.innoq.com/order/v1/sv0")
-package com.innoq.bpel.order.v1.sv0;
--- target/generated/src/main/java/com/innoq/bpel/order/v1/sv1/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://bpel.innoq.com/order/v1/sv1")
-package com.innoq.bpel.order.v1.sv1;
